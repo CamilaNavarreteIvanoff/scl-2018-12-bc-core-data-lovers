ade7ceadfc54033da0bbed4713eaab4d22326bed
CamilaNavarreteIvanoff <cami.ilanavarrete@gmail.com>
2018-12-27 11:44:49 -0300

    funcion de filtrado 1

--- src/data.js
+++ src/data.js
@@ -6,3 +6,46 @@ const example = () => {
 };
 
 window.example = example;
+
+// const TiposDePokemon = pokemon.map(function(tipo){
+//   return pokemon.tipo
+// });
+// window.pokemon.tipo ;
+
+// console.log(TiposDePokemon);
+
+
+//  const datos = POKEMON.pokemon;
+
+//   let tiposDePokemon = POKEMON.pokemon.filter(function(tipos){
+//           return tipos.type.indexOf("Grass", ) !== -1;});
+
+//           console.log(tiposDePokemon)
+          
+ //return tiposDePokemon
+ 
+
+ 
+ function filterData(data, condition)  {
+
+  const tiposDePokemon = data.filter(tipos =>{
+    return tipos.type.indexOf(condition) !== -1;
+
+  })
+  
+  
+    console.log(tiposDePokemon);
+    return tiposDePokemon
+  };
+
+const imagen =[]  
+for (let i=0; i< data.lenght, i++){
+  imagen.push(data[i]["img"]);
+};
+
+
+
+//     let tiposDePokemon = POKEMON.pokemon.filter(function(tipos){
+//        return tipos.type.indexOf.value !== -1;
+    
+// });
