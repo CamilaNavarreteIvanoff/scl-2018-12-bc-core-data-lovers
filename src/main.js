a37406d59c349d3e55d5ace365a872eaf243117e
CamilaNavarreteIvanoff <cami.ilanavarrete@gmail.com>
2018-12-27 17:04:25 -0300

    visualización filtrado

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+  });
+
+
+
 document.getElementById("select2").addEventListener("change",() =>{
     document.getElementById("contenedor").innerHTML = " " 
    
      let tipo =document.getElementById("select2").value
-   for (let i = 0; i < filterData(POKEMON.pokemon , tipo).length ; i++){
+   for (let i = 0; i < filterData(POKEMON.pokemon, tipo).length ; i++){
    
-       document.getElementById("contenedor").innerHTML += `${filterData(POKEMON.pokemon , tipo)[i].name}<br>`
+       document.getElementById("contenedor").innerHTML += `<img src="${filterData(POKEMON.pokemon, tipo)[i].img}" alt="${filterData(POKEMON.pokemon, tipo)[i].img}>"${filterData(POKEMON.pokemon, tipo)[i].name}<br>`;
+      
+     
    }
    });
+
+   
+
+//    document.getElementById("contenedor").innerHTML +=
+//     ` <img src="${filterData(POKEMON.pokemon, "name", document.getElementById("select2").value)[i].img}"   <br>`;
+
+
